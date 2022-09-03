0b929877b319e5e656a78c5ad42c3ab380d6a699
Levi H <levi.heintzelman@gmail.com>
2022-09-03 15:18:43 -0600

    Change folder structure again

--- server.js
+++ server.js
@@ -20,3 +20,8 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);

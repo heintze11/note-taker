483a26bad1ce3dc7569e6a4cd20cb90a309bf074
Levi H <levi.heintzelman@gmail.com>
2022-09-05 11:06:44 -0600

    Add Utils file for fs calls

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,29 @@
 const router = require('express').Router();
+const { readAndAppend, readFromFile } = require('../utils/utils');
+
 
 router.get('/', (req, res) => 
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))  
 );
 
 router.post('/', (req, res) => {
-    
-})
+    const { noteName, note } = req.body;
+    if (noteName && note) {
+        const newNote = {
+            noteName,
+            note,
+          };
+          readAndAppend(newFeedback, './db/feedback.json');
+
+          const response = {
+            status: 'success',
+            body: newFeedback,
+          };
+      
+          res.json(response);
+        } else {
+          res.json('Error in posting feedback');
+        }
+      });
 
 module.exports = router;
